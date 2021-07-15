13749719971a80286002c5ccc618dcc7e6c82606
Christian Jaeger <ch@christianjaeger.ch>
2021-07-15 12:34:15 +0000

    output.h: formatting

--- output.h
+++ output.h
@@ -1,20 +1,22 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
-#ifndef SMALL
-/* Print a string on the screen, wrapping it properly.
- * Like puts() it adds a final line break. */
-void putstring(const char *s);
-#endif
-
 #ifdef SMALL
+
 # define CHROUT 0xFFD2
 # define PUTCHAR(c) { __asm__("lda #%b", (unsigned char) c); __asm__("jsr %w", CHROUT); }
 # define PUTS(s) myputs(s)
 void myputs(const unsigned char *s);
+
 #else
+
+/* Print a string on the screen, wrapping it properly.
+ * Like puts() it adds a final line break. */
+void putstring(const char *s);
+
 # define PUTCHAR(c) putchar(c)
 # define PUTS(s) puts(s)
+
 #endif
 
 #endif
